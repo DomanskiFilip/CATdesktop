ca248ade651b1abcbe24f7d4e14b5238c5fbe0f3
DomanskiFilip <filip_domanski@outlook.com>
2025-05-20 20:51:25 +0100

    tauri + vue CATdesktop initialization

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Import Tauri invoke
+import { invoke } from '@tauri-apps/api/tauri'
+// Import Vue components
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
